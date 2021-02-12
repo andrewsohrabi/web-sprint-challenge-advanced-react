6bc455da550fa3d334b2cd269ba1c08ce28165ca
Andrew Sohrabi <andrewsohrabi@Andrews-MacBook-Pro.local>
2021-02-12 02:09:40 -0800

    met mvp

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
@@ -14,7 +15,32 @@ test("form header renders", () => {
 
 });
 
-// right now no success message appears
 test("form shows success message on submit with form details", () => {
     render(<CheckoutForm />)
+
+    const firstName = screen.getByLabelText(/first name/i);
+    const lastName = screen.getByLabelText(/last name/i);
+    const address = screen.getByLabelText(/address/i);
+    const city = screen.getByLabelText(/city/i);
+    const state = screen.getByLabelText(/state/i);
+    const zip = screen.getByLabelText(/zip/i);
+    const button = screen.getByRole("button", { name: /checkout/i });
+
+    userEvent.type(firstName,"Chuck")
+    userEvent.type(lastName,"Norris")
+    userEvent.type(address,"666 Do Not Come Here Drive")
+    userEvent.type(city,"NYC")
+    userEvent.type(state,"New York")
+    userEvent.type(zip,"66666")
+
+    userEvent.click(button);
+
+    const sucessMessageSnippet = screen.getByText(/You have ordered some plants!/i)
+
+    expect(sucessMessageSnippet).toBeInTheDocument();
+
+    const formDetailsName = screen.getByText(/chuck norris/i)
+
+    expect(formDetailsName).toBeInTheDocument();
+
 });
